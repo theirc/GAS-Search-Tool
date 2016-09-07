5707034db8b4ac66b7d359a7191d0b788b4bc4bf
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2016-09-07 22:25:25 +0200

    Parsing date and using legible column headers.

--- appointment_search/management/commands/import_spreadsheet.py
+++ appointment_search/management/commands/import_spreadsheet.py
@@ -1,11 +1,14 @@
 import tempfile
+from datetime import datetime
+import pytz
+import dateparser
 
 import requests
 import xlrd
 from django.core.management.base import BaseCommand
 
 from appointment_search import models
-from datetime import datetime
+
 
 class Command(BaseCommand):
     help = 'Imports data for the appointment database'
@@ -31,19 +34,23 @@ class Command(BaseCommand):
 
         sample = rows[0] if rows else None
         if sample:
-            if 'registration_number' in sample and 'date' in sample:
+            if len({'Registration Number', 'Office', 'Date'}.intersection(sample.keys())) == 3:
                 for r in rows:
                     appointment, created = models.AppointmentSchedule.objects.get_or_create(
-                        registration_number=str(int(r['registration_number'])))
+                        registration_number=str(int(r['Registration Number'])))
 
-                    date = r['date']
+                    date = r['Date']
                     if type(date) is float:
                         date = datetime(*xlrd.xldate_as_tuple(date, w.datemode))
+                    else:
+                        date = dateparser.parse(date, settings={'DATE_ORDER': 'DMY'})
+                    athens = pytz.timezone("Europe/Athens")
+                    date = athens.localize(date)
 
-                    asylum_office, c = models.AsylumOffice.objects.get_or_create(name=r['asylum_office'])
+                    asylum_office, c = models.AsylumOffice.objects.get_or_create(name=r['Office'])
 
                     appointment.date = date
                     appointment.office = asylum_office
                     appointment.save()
 
-                    print('Imported appointment for case number {}'.format(r['registration_number']))
+                    print('Imported appointment for registration number {}'.format(appointment.registration_number))
