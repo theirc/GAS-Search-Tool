7a27d5e02cacfea28d7a45254bc2df601fceadfd
Rey Rodrigues <rodriguesreynaldo@gmail.com>
2016-09-08 10:12:08 +0200

    Import

--- appointment_search/management/commands/import_spreadsheet.py
+++ appointment_search/management/commands/import_spreadsheet.py
@@ -1,5 +1,6 @@
+from __future__ import print_function, unicode_literals
 import tempfile
-from datetime import datetime
+from datetime import datetime,date as _date
 import pytz
 import dateparser
 
@@ -28,29 +29,33 @@ class Command(BaseCommand):
 
         sheet = w.sheet_by_index(0)
 
-        header = [sheet.cell_value(0, i) for i in range(0, sheet.ncols)]
+        header = [unicode(sheet.cell_value(0, i)).lower() for i in range(0, sheet.ncols)]
         rows = [dict(zip(header, [sheet.cell_value(j, i) for i in range(0, sheet.ncols)])) for j in
                 range(1, sheet.nrows)]
 
         sample = rows[0] if rows else None
+        print ('Loaded spreadsheet')
         if sample:
-            if len({'Registration Number', 'Office', 'Date'}.intersection(sample.keys())) == 3:
+            if len({'refugeeid', 'as office eng', 'date', 'hour of day'}.intersection(sample.keys())) == 4:
                 for r in rows:
                     appointment, created = models.AppointmentSchedule.objects.get_or_create(
-                        registration_number=str(int(r['Registration Number'])))
-
-                    date = r['Date']
-                    if type(date) is float:
-                        date = datetime(*xlrd.xldate_as_tuple(date, w.datemode))
+                        registration_number=str(int(r['refugeeid'])))
+                    if type(r['date']) is float:
+                        date = xlrd.xldate_as_tuple(r['date'], w.datemode)
+                        date = "{} {}".format(_date(*date[0:3]).isoformat(), r['hour of day'])
+                        date = dateparser.parse(date)
                     else:
+                        date = "{} {}".format(r['date'], r['hour of day'])
                         date = dateparser.parse(date, settings={'DATE_ORDER': 'DMY'})
+
                     athens = pytz.timezone("Europe/Athens")
                     date = athens.localize(date)
 
-                    asylum_office, c = models.AsylumOffice.objects.get_or_create(name=r['Office'])
+                    asylum_office, c = models.AsylumOffice.objects.get_or_create(name=r['as office eng'])
 
                     appointment.date = date
                     appointment.office = asylum_office
                     appointment.save()
 
                     print('Imported appointment for registration number {}'.format(appointment.registration_number))
+        print('Import complete')
