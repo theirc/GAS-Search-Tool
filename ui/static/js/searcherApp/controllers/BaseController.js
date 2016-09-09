8150943ad474ecc90cb0c1aba843fda913cf79fd
bklukaczewski <bklukaczewski@gmail.com>
2016-09-09 09:57:29 +0200

    Add change language logic; style loading overlay

--- ui/static/js/searcherApp/controllers/BaseController.js
+++ ui/static/js/searcherApp/controllers/BaseController.js
@@ -1,6 +1,10 @@
-angular.module('searcherApp').controller('BaseController', function ($rootScope, LoadingOverlayService, languages) {
+angular.module('searcherApp').controller('BaseController', function ($rootScope, LoadingOverlayService, $state,
+                                                                     languages, $translate, $cookies, $templateCache) {
     var vm = this;
+    vm.isCookiePolicyAccepted = $cookies.get('cookiePolicy');
     vm.languages = languages;
+    vm.language = $translate.proposedLanguage() || $translate.use();
+    vm.isRTL = vm.language && !(vm.language.code === 'en' || vm.language.code === 'el');
 
     var deregisterStateChangeStartHandler = $rootScope.$on('$stateChangeStart', function () {
         LoadingOverlayService.start();
@@ -14,4 +18,20 @@ angular.module('searcherApp').controller('BaseController', function ($rootScope,
         deregisterStateChangeStartHandler();
         deregisterStateChangeEndHandler();
     });
+
+    vm.acceptCookiePolicy = function () {
+        var now = new Date();
+        var exp = new Date(now.getFullYear() + 1, now.getMonth(), now.getDate());
+        vm.isCookiePolicyAccepted = true;
+        $cookies.put('cookiePolicy', 'true', {'expires': exp});
+    };
+
+    vm.changeLanguage = function (language) {
+        console.log('clicked');
+        vm.isRTL = !(language.code === 'en' || language.code === 'el');
+        vm.language = language;
+        $translate.use(language.code);
+        $templateCache.removeAll();
+        $state.reload();
+    };
 });
