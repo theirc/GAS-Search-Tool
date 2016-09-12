c2825731c6a53633733555acf1dbd160200bb5ae
knowak <knowak@soldevelo.com>
2016-09-12 13:04:42 +0200

    Support rtl in forward/back buttons

--- ui/static/js/searcherApp/directives/backButton.js
+++ ui/static/js/searcherApp/directives/backButton.js
@@ -1,9 +1,14 @@
-angular.module('searcherApp').directive('backButton', function() {
+angular.module('searcherApp').directive('backButton', function () {
     return {
         restrict: 'E',
         scope: {
             direction: '@'
         },
-        templateUrl: '/partials/directives/back-button.html'
+        link: function (scope) {
+            var forwardButton = '/partials/directives/forward-button.html';
+            var backButton = '/partials/directives/back-button.html';
+            scope.contentUrl = scope.direction == 'ltr' ? backButton : forwardButton;
+        },
+        template: '<div ng-include="contentUrl"></div>'
     };
 });
