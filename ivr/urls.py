b07c69e8177e4f346e2b2bffbc6a6be6c67bdb4c
Yali Wu <ywu@twilio.com>
2016-11-15 22:30:50 -0800

    template for different ivr endpoints

--- ivr/urls.py
+++ ivr/urls.py
@@ -3,5 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.test, name='test'),
+    url(r'incoming', views.incoming, name='incoming'),
+    url(r'registration', views.registration, name='registration'),
+    url(r'appointment', views.appointment, name='appointment'),
+    url(r'complete_menu', views.complete_menu, name='complete_menu'),
 ]
--- ivr/views.py
+++ ivr/views.py
@@ -3,14 +3,38 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from appointment_search.models import AppointmentSchedule
 
+
 def time_of_day(hour):
     if hour < 12:
         return 'MORNING'
     else:
         return 'AFTERNOON'
 
+
 @csrf_exempt
-def index(request):
+def incoming(request):
+    # Language Selection Menu
+    return HttpResponse("TwiML for language selection manual")
+
+
+@csrf_exempt
+def registration(request):
+    # Registration Prompt
+    return HttpResponse("TwiML for registration prompt")
+
+
+@csrf_exempt
+def appointment(request):
+    return HttpResponse("TwiML for playing back detailed appointment information")
+
+
+@csrf_exempt
+def complete_menu(request):
+    return HttpResponse("TwiML for redirect based on selection")
+
+# This is a test method to demon how to retrieve the appointment details for existing dal layer
+@csrf_exempt
+def test(request):
     registration_number = request.POST.get('registration_number')
     appointment_details = _get_appointment_details(registration_number)
 
@@ -31,8 +55,8 @@ def index(request):
 
 def _get_appointment_details(registration_number):
     appointments = AppointmentSchedule.objects.filter(registration_number=registration_number)
-    appointment = appointments[0]
-    datetime = appointment.date
+    appt = appointments[0]
+    datetime = appt.date
     date = datetime.strftime('%d-%m-%Y')
     hour = datetime.strftime("%H:%M")
     am_pm = appointment.date.strftime("%p")
